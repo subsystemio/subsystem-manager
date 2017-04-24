0c39b1ed61509777808c6ac957fc23ef5c3abd60
Dominic Cassidy <dominic.cassidy93@gmail.com>
2017-04-24 23:08:19 +0100

    --Changes * Now using "gopkg.in/gin-gonic/gin.v1" to provide simple RESTful behaviour. * Heartbeat and Registration by SubSystem now supported.
    Signed-off-by: Dominic Cassidy <dominic.cassidy93@gmail.com>

--- main.go
+++ main.go
@@ -1,43 +1,66 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"log"
+
+	"github.com/subsystemio/subsystem"
+	"gopkg.in/gin-gonic/gin.v1"
 )
 
-//HashData describes expected hash results of source and build files.
-type HashData struct {
-	Source string `json:"source"`
-	Build  string `json:"build"`
-}
-
-//RequirementsData outlines required running subsystems.
-type RequirementsData struct {
-	Name    string `json:"name"`
-	Version string `json:"version"`
-}
-
-//APIData describes inputs and outputs of subsystem.
-type APIData struct {
-	Read  []string `json:"read"`
-	Write []string `json:"write"`
-}
-
-//SubSystem main structure.
-type SubSystem struct {
-	Name         string             `json:"name"`
-	Version      string             `json:"version"`
-	Hash         HashData           `json:"hash"`
-	API          APIData            `json:"api"`
-	Requirements []RequirementsData `json:"requirements"`
-	Limit        int                `json:"limit"`
-}
+var (
+	subsystems map[string]SubSystem.SubSystem
+)
 
 func main() {
-	file, _ := ioutil.ReadFile("./subsystem.json")
+	subsystems = make(map[string]SubSystem.SubSystem)
 
-	s := SubSystem{}
-	json.Unmarshal(file, &s)
-	fmt.Printf("Found: %v\n", s)
+	r := gin.Default()
+
+	v1 := r.Group("/v1")
+	{
+		v1.POST("/subsystems", func(c *gin.Context) {
+			var d SubSystem.SubSystemData
+			if c.BindJSON(&d) == nil {
+				log.Printf("Data %v", d)
+
+				h := SubSystem.HealthCheck{
+					Name: d.Name,
+					Key:  "password",
+				}
+				s := SubSystem.SubSystem{
+					Data:   d,
+					Health: h,
+				}
+
+				subsystems[d.Name] = s
+
+				c.JSON(200, gin.H{
+					"name": s.Health.Name,
+					"key":  s.Health.Key,
+				})
+			} else {
+				c.JSON(500, gin.H{
+					"error": "Failed to load SubSystem data.",
+				})
+			}
+		})
+		v1.POST("/health", func(c *gin.Context) {
+			var d SubSystem.HealthCheck
+			if c.BindJSON(&d) == nil {
+				if val, ok := subsystems[d.Name]; ok {
+					if val.Health.Key == d.Key {
+						c.String(200, "Ba-Bump")
+						return
+					}
+				}
+
+				c.String(500, "Bad SubSystem or Key")
+
+			} else {
+				c.String(500, "Failed to bind healthcheck")
+			}
+		})
+	}
+
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
